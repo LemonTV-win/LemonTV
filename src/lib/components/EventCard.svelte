470c41556ffbb55c55948d08e2e9ada37d9a91c8
mkpoli <git@mkpo.li>
2025-04-18 18:54:40 +0900

    feat: adjust EventCard sizing

--- src/lib/components/EventCard.svelte
+++ src/lib/components/EventCard.svelte
@@ -6,7 +6,7 @@
 
 <a
 	href="/events/{event.id}"
-	class="flex items-center gap-2 border-b-1 border-gray-500 bg-gray-800 shadow-2xl"
+	class="flex min-h-32 items-center gap-2 border-b-1 border-gray-500 bg-gray-800 shadow-2xl"
 >
 	<img src={event.image} alt={event.name} class="w-full max-w-64" />
 	<span class="p-4 text-2xl text-white">{event.name}</span>
