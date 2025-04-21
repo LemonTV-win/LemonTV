91ef3d877eaab101778e96ee0183dfa5787bf936
mkpoli <git@mkpo.li>
2025-04-21 19:20:13 +0900

    feat: add news page

---
+++ src/routes/news/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { m } from '$lib/paraglide/messages.js';
+</script>
+
+<main class="mx-auto max-w-screen-lg">
+	<h1 class="my-10 text-2xl font-bold">{m.news()}</h1>
+	TBD
+</main>
