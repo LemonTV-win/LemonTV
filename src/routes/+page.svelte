76e663edc507ac08f7cb2c7dd2802d738dac4d9c
mkpoli <git@mkpo.li>
2025-05-03 15:51:21 +0900

    refactor: use EventCard for upcoming as well

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,15 +22,7 @@
 			<h3 class="my-4 text-xl font-bold">{m.upcoming()}</h3>
 			<ul class="flex flex-col gap-2">
 				{#each upcomingEvents as event}
-					<li>
-						<a
-							href="/events/{event.id}"
-							class="flex flex-col items-center gap-2 border-b-1 border-gray-500 bg-gray-800 shadow-2xl sm:flex-row"
-						>
-							<img src={event.image} alt={event.name} class="w-full max-w-full sm:max-w-64" />
-							<span class="p-4 text-xl text-white sm:text-2xl">{event.name}</span>
-						</a>
-					</li>
+					<EventCard {event} />
 				{/each}
 			</ul>
 		{/if}
