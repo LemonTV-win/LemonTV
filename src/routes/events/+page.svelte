0d8b6e4165d2252a19b2e7785492de9559df49d7
mkpoli <git@mkpo.li>
2025-05-04 09:38:43 +0900

    feat: categorize the events page

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,14 +1,38 @@
 <script lang="ts">
 	import EventCard from '$lib/components/EventCard.svelte';
+	import { m } from '$lib/paraglide/messages';
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
+
+	let ongoingEvents = data.events.filter((event) => event.status === 'live');
+	let upcomingEvents = data.events.filter((event) => event.status === 'upcoming');
+	let finishedEvents = data.events.filter((event) => event.status === 'finished');
 </script>
 
 <main class="mx-auto max-w-screen-lg md:px-4">
 	<h1 class="mx-0 my-10 text-2xl font-bold md:mx-4">Events</h1>
+
 	<ul>
-		{#each data.events as event}
+		{#each ongoingEvents as event}
+			<li>
+				<EventCard {event} />
+			</li>
+		{/each}
+	</ul>
+
+	{#if upcomingEvents.length > 0}
+		<h2 class="my-4 text-xl font-bold">{m.upcoming()}</h2>
+		<ul class="flex flex-col gap-2">
+			{#each upcomingEvents as event}
+				<EventCard {event} />
+			{/each}
+		</ul>
+	{/if}
+
+	<h3 class="my-4 text-xl font-bold">{m.finished()}</h3>
+	<ul>
+		{#each finishedEvents as event}
 			<li>
 				<EventCard {event} />
 			</li>
