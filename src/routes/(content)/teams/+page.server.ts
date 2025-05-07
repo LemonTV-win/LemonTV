dd3d422828a81c20ebab84263d9c3e5ad202b2e7
mkpoli <git@mkpo.li>
2025-05-07 11:20:37 +0900

    feat: fix teams player retrieval

--- src/routes/(content)/teams/+page.server.ts
+++ src/routes/(content)/teams/+page.server.ts
@@ -1,14 +1,15 @@
 import type { PageServerLoad } from './$types';
 import { getTeams, type CompiledTeam } from '$lib/data';
-import { getPlayer } from '$lib/server/data/players';
+import { getPlayer, getPlayers } from '$lib/server/data/players';
 
-export const load: PageServerLoad = () => {
+export const load: PageServerLoad = async () => {
+	const players = await getPlayers();
 	return {
 		teams: getTeams().map((team) => ({
 			...team,
-			players: team.players?.map((player) => getPlayer(player)),
-			substitutes: team.substitutes?.map((player) => getPlayer(player)),
-			former: team.former?.map((player) => getPlayer(player))
+			players: team.players?.map((player) => players.find((p) => p.slug === player)),
+			substitutes: team.substitutes?.map((player) => players.find((p) => p.slug === player)),
+			former: team.former?.map((player) => players.find((p) => p.slug === player))
 		})) as (CompiledTeam & { wins: number })[]
 	};
 };
