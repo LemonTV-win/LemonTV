eed6ad8adc3d57c9c32ee2981941610cc394dc8a
mkpoli <git@mkpo.li>
2025-05-08 08:15:56 +0900

    refactor: use json to manage

--- src/routes/(content)/community/+page.svelte
+++ src/routes/(content)/community/+page.svelte
@@ -1,5 +1,55 @@
 <script lang="ts">
 	import { m } from '$lib/paraglide/messages.js';
+
+	const discordLinks = [
+		{
+			title: 'Official Strinova Discord Server',
+			url: 'https://discord.com/invite/strinova',
+			additionalLink: {
+				text: '#tournament-chat',
+				url: 'https://discord.com/channels/1182952140684136470/1320683196698066954'
+			}
+		},
+		{
+			title: 'Strinova EMEA Discord Server',
+			url: 'https://discord.gg/jE7U2QF7wu'
+		},
+		{
+			title: 'カラビヤウの民 (Japanese)',
+			url: 'https://discord.gg/SZuKAqNMY2'
+		}
+	];
+
+	const toolsAndResources = [
+		{
+			title: 'Strinova Wiki',
+			url: 'https://strinova.org/wiki/Main_Page'
+		},
+		{
+			title: '卡拉彼丘WIKI',
+			url: 'https://wiki.biligame.com/klbq/%E9%A6%96%E9%A1%B5'
+		},
+		{
+			title: 'Strinova Map Assistant',
+			url: 'https://strinova.fsltech.cn/'
+		},
+		{
+			title: 'カラビヤウ（ストリノヴァ）中国語単語帳',
+			url: 'https://docs.google.com/spreadsheets/d/1H-C2nFi11dSbqhQ6-_3QgBjw99LslWnazGd_tSvWKGM/edit?usp=sharing',
+			description: '(Chinese-Japanese glossary of Calabiyau/Strinova)'
+		},
+		{
+			title: 'Strinova Map Callouts (English, nbound version)',
+			url: 'https://drive.google.com/drive/folders/1S7XA8S66zZiliM5vBmeqdIPrXUKn6kWL'
+		}
+	];
+
+	const speedrunLinks = [
+		{
+			title: 'Speedrun.com (Strinova)',
+			url: 'https://www.speedrun.com/Strinova'
+		}
+	];
 </script>
 
 <svelte:head>
@@ -8,78 +58,50 @@
 
 <main class="mx-auto max-w-screen-lg">
 	<h1 class="my-10 text-2xl font-bold">{m.community()}</h1>
+
 	<h2 class="text-xl font-bold">Discord</h2>
 	<ul class="m-4 flex flex-col flex-wrap gap-4 p-2">
-		<li class="flex items-center gap-2 rounded-md bg-gray-800 p-2">
-			<a
-				href="https://discord.com/invite/strinova"
-				class="text-yellow-300 hover:text-yellow-400 hover:underline"
-				target="_blank">Official Strinova Discord Server</a
-			>
-			-
-			<a href="https://discord.com/channels/1182952140684136470/1320683196698066954"
-				>#tournament-chat</a
-			>
-		</li>
-		<li class="flex items-center gap-2 rounded-md bg-gray-800 p-2">
-			<a
-				href="https://discord.gg/jE7U2QF7wu"
-				class="text-yellow-300 hover:text-yellow-400 hover:underline"
-				target="_blank">Strinova EMEA Discord Server</a
-			>
-		</li>
-		<li class="flex items-center gap-2 rounded-md bg-gray-800 p-2">
-			<a
-				href="https://discord.gg/SZuKAqNMY2"
-				class="text-yellow-300 hover:text-yellow-400 hover:underline"
-				target="_blank">カラビヤウの民 (Japanese)</a
-			>
-		</li>
+		{#each discordLinks as link}
+			<li class="flex items-center gap-2 rounded-md bg-gray-800 p-2">
+				<a
+					href={link.url}
+					class="text-yellow-300 hover:text-yellow-400 hover:underline"
+					target="_blank">{link.title}</a
+				>
+				{#if link.additionalLink}
+					-
+					<a href={link.additionalLink.url}>{link.additionalLink.text}</a>
+				{/if}
+			</li>
+		{/each}
 	</ul>
+
 	<h2 class="text-xl font-bold">{m.tools_resources()}</h2>
 	<ul class="m-4 flex flex-col flex-wrap gap-4 p-2">
-		<li class="flex items-center gap-2 rounded-md bg-gray-800 p-2">
-			<a
-				href="https://strinova.org/wiki/Main_Page"
-				class="text-yellow-300 hover:text-yellow-400 hover:underline">Strinova Wiki</a
-			>
-		</li>
-		<li class="flex items-center gap-2 rounded-md bg-gray-800 p-2">
-			<a
-				href="https://wiki.biligame.com/klbq/%E9%A6%96%E9%A1%B5"
-				class="text-yellow-300 hover:text-yellow-400 hover:underline">卡拉彼丘WIKI</a
-			>
-		</li>
-		<li class="flex items-center gap-2 rounded-md bg-gray-800 p-2">
-			<a
-				href="https://strinova.fsltech.cn/"
-				class="text-yellow-300 hover:text-yellow-400 hover:underline"
-				target="_blank">Strinova Map Assistant</a
-			>
-		</li>
-		<li class="flex items-center gap-2 rounded-md bg-gray-800 p-2">
-			<a
-				href="https://docs.google.com/spreadsheets/d/1H-C2nFi11dSbqhQ6-_3QgBjw99LslWnazGd_tSvWKGM/edit?usp=sharing"
-				class="text-yellow-300 hover:text-yellow-400 hover:underline"
-				target="_blank">カラビヤウ（ストリノヴァ）中国語単語帳</a
-			> (Chinese-Japanese glossary of Calabiyau/Strinova)
-		</li>
-		<li class="flex items-center gap-2 rounded-md bg-gray-800 p-2">
-			<a
-				href="https://drive.google.com/drive/folders/1S7XA8S66zZiliM5vBmeqdIPrXUKn6kWL"
-				class="text-yellow-300 hover:text-yellow-400 hover:underline"
-				>Strinova Map Callouts (English, nbound version)</a
-			>
-		</li>
+		{#each toolsAndResources as resource}
+			<li class="flex items-center gap-2 rounded-md bg-gray-800 p-2">
+				<a
+					href={resource.url}
+					class="text-yellow-300 hover:text-yellow-400 hover:underline"
+					target="_blank">{resource.title}</a
+				>
+				{#if resource.description}
+					{resource.description}
+				{/if}
+			</li>
+		{/each}
 	</ul>
+
 	<h2 class="text-xl font-bold">Speedrun</h2>
 	<ul class="m-4 flex flex-col flex-wrap gap-4 p-2">
-		<li class="flex items-center gap-2 rounded-md bg-gray-800 p-2">
-			<a
-				href="https://www.speedrun.com/Strinova"
-				class="text-yellow-300 hover:text-yellow-400 hover:underline"
-				target="_blank">Speedrun.com (Strinova)</a
-			>
-		</li>
+		{#each speedrunLinks as link}
+			<li class="flex items-center gap-2 rounded-md bg-gray-800 p-2">
+				<a
+					href={link.url}
+					class="text-yellow-300 hover:text-yellow-400 hover:underline"
+					target="_blank">{link.title}</a
+				>
+			</li>
+		{/each}
 	</ul>
 </main>
