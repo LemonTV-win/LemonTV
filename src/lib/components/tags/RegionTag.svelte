979f5d98e29723b82a195c88ca327316e3992987
mkpoli <git@mkpo.li>
2025-05-12 10:58:18 +0900

    feat: add an ad-hoc solution to region names

--- src/lib/components/tags/RegionTag.svelte
+++ src/lib/components/tags/RegionTag.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
 	import IconParkSolidLocal from '~icons/icon-park-solid/local';
 	let { region, class: className }: { region: string; class?: string } = $props();
+
+	const REGION_TAGS = {
+		APAC: 'APAC',
+		EU: 'Europe',
+		NA: 'America',
+		CN: 'China'
+	};
 </script>
 
 <div class={['flex items-center gap-2 rounded-sm bg-gray-700/50 px-2 py-1', className]}>
 	<IconParkSolidLocal class="h-4 w-4" />
-	<span>{region}</span>
+	<span>{REGION_TAGS[region as keyof typeof REGION_TAGS] || region}</span>
 </div>
