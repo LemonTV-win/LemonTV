cda1bd4cab1c01fe7cb2046e0e9664a29ff40d20
mkpoli <git@mkpo.li>
2025-05-29 00:32:33 +0900

    feat: allow empty capacity

--- src/routes/(user)/admin/events/+page.server.ts
+++ src/routes/(user)/admin/events/+page.server.ts
@@ -93,7 +93,7 @@ export const actions = {
 			!eventData.region ||
 			!eventData.image ||
 			!eventData.status ||
-			!eventData.capacity ||
+			// !eventData.capacity ||
 			!eventData.date
 		) {
 			return fail(400, {
