20eb380ca948ad7b5280c779f59b22b2acb14598
mkpoli <git@mkpo.li>
2025-06-14 01:36:13 +0900

    chore: add knip ignores

--- knip.config.ts
+++ knip.config.ts
@@ -3,7 +3,17 @@ import type { KnipConfig } from 'knip';
 const config: KnipConfig = {
 	paths: {
 		'~icons/*': ['node_modules/unplugin-icons/types/svelte.d.ts']
-	}
+	},
+	ignore: ['drizzle-dev.config.ts', 'src/hooks.ts'],
+	ignoreDependencies: [
+		'@fontsource/*',
+		'@fontsource-variable/*',
+		'@infolektuell/noto-color-emoji',
+		'@iconify/json',
+		'@tailwindcss/forms'
+	],
+	ignoreBinaries: ['turso'],
+	ignoreUnresolved: ['$env/dynamic/private', '$env/static/private']
 };
 
 export default config;
