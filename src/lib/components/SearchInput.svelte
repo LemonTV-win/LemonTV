1c4716b4562631e5acea445e97b2e2eebb7fffe9
mkpoli <git@mkpo.li>
2025-07-30 10:44:48 +0900

    feat: improve focus styles in SearchInput component for better accessibility and user experience

--- src/lib/components/SearchInput.svelte
+++ src/lib/components/SearchInput.svelte
@@ -9,14 +9,14 @@
 </script>
 
 <div
-	class="relative flex w-full max-w-64 items-center gap-2 rounded-md border-white/30 bg-gradient-to-br from-slate-600/60 to-slate-800 px-4 text-gray-400 shadow-2xl ring-1 ring-white/30 backdrop-blur-lg transition-all duration-200"
+	class="focus-within:ring-opacity-100 relative flex w-full max-w-64 items-center gap-2 rounded-md border-white/30 bg-gradient-to-br from-slate-600/60 to-slate-800 px-4 text-gray-400 shadow-2xl ring-1 ring-white/30 backdrop-blur-lg transition-all duration-200 focus-within:text-white focus-within:ring-2 focus-within:ring-white"
 >
 	<MaterialSymbolsSearchRounded class="h-6 w-6" />
 	<input
 		type="search"
 		placeholder="Search"
 		bind:value={search}
-		class="w-full border-none bg-transparent p-2 ring-0 outline-none focus:outline-none"
+		class="w-full border-none bg-transparent p-2 ring-0 outline-none focus:text-white focus:outline-none"
 	/>
 	<span class="text-xs text-gray-500">{filtered}/{total}</span>
 </div>
