29710c486aaf425ee3b474c78a43f8b76ad871d7
mkpoli <git@mkpo.li>
2025-08-14 05:44:11 +0900

    i18n: add batch import translations in multiple languages for improved user interface

--- src/routes/(user)/admin/players/+page.svelte
+++ src/routes/(user)/admin/players/+page.svelte
@@ -287,7 +287,7 @@
 					class="rounded-md bg-slate-700 px-4 py-2 font-medium text-white hover:bg-slate-600"
 					onclick={handleBatchImport}
 				>
-					{m.import_json()}
+					{m['editing.batch.batch_import']()}
 				</button>
 			{/if}
 			<button
--- src/routes/(user)/admin/teams/+page.svelte
+++ src/routes/(user)/admin/teams/+page.svelte
@@ -217,7 +217,7 @@
 					class="rounded-md bg-slate-700 px-4 py-2 font-medium text-white hover:bg-slate-600"
 					onclick={() => (showBatchImportModal = true)}
 				>
-					Batch Import
+					{m['editing.batch.batch_import']()}
 				</button>
 			{/if}
 			<button
