5e1f03e77aa952fdd67416838e7d1d50809bb58b
mkpoli <git@mkpo.li>
2025-08-22 13:02:22 +0900

    feat: fix env detection in scripts/lib

--- scripts/lib/db.ts
+++ scripts/lib/db.ts
@@ -2,16 +2,19 @@ import { drizzle } from 'drizzle-orm/libsql';
 import { createClient, type Client } from '@libsql/client';
 import * as schema from '../../src/lib/server/db/schema';
 
-console.info('[Scripts] Syncing database...');
 let client: Client;
 
-if (import.meta.env.DEV) {
-	console.info('[Scripts] Development environment detected');
+console.info('[Scripts][Lib][DB] Connecting to database...');
+
+console.info('[Scripts][Lib][DB] DEV:', import.meta.env.DEV);
+
+if (import.meta.env.DEV === 'true') {
+	console.info('[Scripts][Lib][DB] Development environment detected');
 	client = createClient({
 		url: 'file:./dev.db'
 	});
 } else {
-	console.info('[Scripts] Production environment detected');
+	console.info('[Scripts][Lib][DB] Production environment detected');
 	if (!import.meta.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 	if (!import.meta.env.DEV && !import.meta.env.DATABASE_AUTH_TOKEN)
 		throw new Error('DATABASE_AUTH_TOKEN is not set');
@@ -21,7 +24,7 @@ if (import.meta.env.DEV) {
 		authToken: import.meta.env.DATABASE_AUTH_TOKEN
 	});
 }
-console.info('[Scripts] LibSQL Client created');
+console.info('[Scripts][Lib][DB] LibSQL Client created');
 
 export const db = drizzle(client, { schema });
-console.info('[Scripts] Database connected');
+console.info('[Scripts][Lib][DB] Database connected');
